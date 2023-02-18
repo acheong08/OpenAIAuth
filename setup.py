a1386eae7df23b146cc074c85430b9931310c03c
Antonio Cheong <acheong@student.dalat.org>
2023-02-18 20:31:47 +0800

    move

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     package_dir={"": "src"},
     url="https://github.com/acheong08/OpenAIAuth",
     install_requires=[
-        "tls-client",
+        "requests",
     ],
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
