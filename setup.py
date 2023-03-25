fea6391543d5cdaeb70ee4e348e9c5f8c1151a9b
CoolPlayLin <CoolPlayLin@qq.com>
2023-03-25 12:16:50 +0800

    fix something

--- setup.py
+++ setup.py
@@ -12,9 +12,24 @@ setup(
     package_dir={"": "src"},
     py_modules=["OpenAIAuth"],
     url="https://github.com/acheong08/OpenAIAuth",
+    project_urls={
+        "Bug Report": "https://github.com/acheong08/OpenAIAuth/issues/new"
+    },
     install_requires=[
         "requests",
     ],
-    long_description=open("README.md", encoding="utf-8").read(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    long_description=open("README.md", "rt", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
 )
