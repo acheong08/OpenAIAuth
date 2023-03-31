4c8db6f71ac502187ffa1de72a7546b7728afdbd
Antonio Cheong <acheong@student.dalat.org>
2023-03-31 19:48:40 +0800

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="OpenAIAuth",
-    version="0.3.5",
+    version="0.3.6",
     license="MIT",
     author="Rawand Ahmed Shaswar and Antonio Cheong",
     author_email="acheong@student.dalat.org",
@@ -12,9 +12,7 @@ setup(
     package_dir={"": "src"},
     py_modules=["OpenAIAuth"],
     url="https://github.com/acheong08/OpenAIAuth",
-    project_urls={
-        "Bug Report": "https://github.com/acheong08/OpenAIAuth/issues/new"
-    },
+    project_urls={"Bug Report": "https://github.com/acheong08/OpenAIAuth/issues/new"},
     install_requires=[
         "requests",
     ],
