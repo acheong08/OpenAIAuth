73a0142e194096d9616d556c803fbc6a9bbbd366
Antonio <acheong@student.dalat.org>
2023-05-27 09:57:22 +0800

    pypi

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name="OpenAIAuth",
+    version="1.0.0",
+    license="MIT",
+    author="pengzhile",
+    author_email="<Unknown>",
+    description="OpenAI Authentication Reverse Engineered",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=["OpenAIAuth"],
+    url="https://github.com/acheong08/OpenAIAuth",
+    project_urls={"Bug Report": "https://github.com/acheong08/OpenAIAuth/issues/new"},
+    install_requires=[
+        "requests",
+    ],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    long_description=open("README.md", "rt", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+)
