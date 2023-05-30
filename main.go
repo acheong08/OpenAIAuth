e912a5b7c36a5fd1d834a4e594e9aa454dae8e8a
Antonio <acheong@student.dalat.org>
2023-05-30 12:19:47 +0800

    refactor

--- main.go
+++ main.go
@@ -10,15 +10,15 @@ import (
 func main() {
 	auth := auth.NewAuthenticator(os.Getenv("OPENAI_EMAIL"), os.Getenv("OPENAI_PASSWORD"), os.Getenv("PROXY"))
 	err := auth.Begin()
-	if err.Error != nil {
+	if err != nil {
 		println("Error: " + err.Details)
 		println("Location: " + err.Location)
 		println("Status code: " + fmt.Sprint(err.StatusCode))
 		println("Embedded error: " + err.Error.Error())
 		return
 	}
-	token, err := auth.GetAccessToken()
-	if err.Error != nil {
+	token := auth.GetAccessToken()
+	if err != nil {
 		println("Error: " + err.Details)
 		println("Location: " + err.Location)
 		println("Status code: " + fmt.Sprint(err.StatusCode))
