55a0f33f1057355f07bcfd72e9e865d14c0816f9
Antonio <acheong@student.dalat.org>
2023-06-10 03:34:08 +0800

    show PUID

--- main.go
+++ main.go
@@ -18,17 +18,17 @@ func main() {
 		println("Embedded error: " + err.Error.Error())
 		return
 	}
-	if os.Getenv("PROXY") != "" {
-		puid, err := auth.GetPUID()
-		if err != nil {
-			println("Error: " + err.Details)
-			println("Location: " + err.Location)
-			println("Status code: " + fmt.Sprint(err.StatusCode))
-			println("Embedded error: " + err.Error.Error())
-			return
-		}
-		println("PUID: " + puid)
+	// if os.Getenv("PROXY") != "" {
+	puid, err := auth.GetPUID()
+	if err != nil {
+		println("Error: " + err.Details)
+		println("Location: " + err.Location)
+		println("Status code: " + fmt.Sprint(err.StatusCode))
+		println("Embedded error: " + err.Error.Error())
+		return
 	}
+	println("PUID: " + puid)
+	// }
 	// JSON encode auth.GetAuthResult()
 	result := auth.GetAuthResult()
 	result_json, _ := json.Marshal(result)
