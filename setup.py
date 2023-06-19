aeff082185246bf2ddf87a190dc0f17b6c8abf22
Antonio Cheong <acheong@student.dalat.org>
2023-06-19 16:26:31 +0800

    1.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="OpenAIAuth",
-    version="1.0.2",
+    version="1.0.3",
     license="MIT",
     author="pengzhile",
     author_email="acheong@student.dalat.org",
